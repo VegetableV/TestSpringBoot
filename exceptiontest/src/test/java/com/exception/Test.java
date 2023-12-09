66ac3640dae11e820c3d781ed329ec622509769f
VegetableV <hxy1377883453@163.com>
2023-12-09 19:45:34 +0800

    异常处理-add

--- exceptiontest/src/test/java/com/exception/Test.java
+++ exceptiontest/src/test/java/com/exception/Test.java
@@ -10,8 +10,7 @@ import java.util.Set;
 public class Test {
     public static void main(String[] args) {
         Guest guest=new Guest();
-        guest.setName("");
-        guest.setRole("");
+
         Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
         Validator validator1 = Validation.byDefaultProvider().configure().addProperty("hibernate.validator.fail_fast", "true").buildValidatorFactory().getValidator();
         Set<ConstraintViolation<Guest>> validates = validator.validate(guest);
